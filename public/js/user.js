4eea7c7510a4b7aa58a73f239fc708c30c73d3e0
TimRChen <527410313@qq.com>
2017-05-03 11:40:03 +0800

    add user.js

---
+++ public/js/user.js
@@ -0,0 +1,19 @@
+$(function() {
+    $('.del').click(function(e) {
+        let target = $(e.target);
+        let id = target.data('id');
+        let tr = $(`.item-id-${id}`);
+
+        $.ajax({
+            type: 'DELETE',
+            url: `/admin/user/list?id=${id}`,
+        })
+        .done(function(results) {
+            if (results.success === 1) {
+                if (tr.length > 0) {
+                    tr.remove();
+                }
+            }
+        });
+    });
+});
