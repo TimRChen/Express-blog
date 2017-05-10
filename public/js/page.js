78f71f7e50b842dd1127f5172de223a0703e9adb
TimRChen <527410313@qq.com>
2017-05-10 13:55:40 +0800

    update request next page

--- public/js/page.js
+++ public/js/page.js
@@ -1,24 +1,24 @@
 $(function() {
     $('#previous').click(function(e) {
         $.ajax({
-            type: 'POST',
-            url: `page?page=previous`,
-        })
-        .done(function(results) {
-            if (results.success === 1) {
-                //.. code
-            }
+            type: 'GET',
+            url: `?page=previous`,
         });
+        // .done(function(results) {
+        //     if (results.success === 1) {
+        //         //.. code
+        //     }
+        // });
     });
     $('#next').click(function(e) {
         $.ajax({
-            type: 'POST',
-            url: `page?page=next`,
-        })
-        .done(function(results) {
-            if (results.success === 1) {
-                //.. code
-            }
+            type: 'GET',
+            url: `?page=next`,
         });
+        // .done(function(results) {
+        //     if (results.success === 1) {
+        //         //.. code
+        //     }
+        // });
     });
 });
